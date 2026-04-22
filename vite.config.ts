84cb09a00cd206380d60eb868db185aa7ee84c68
HonstinII <a374361365@qq.com>
2026-04-22 16:09:49 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/Hubto/',
+  base: '/https://github.com/HonstinII/Hubto',
   plugins: [react()],
   resolve: {
     alias: {
